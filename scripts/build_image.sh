06369cc61b1638382d5c9cc96a7d3de0ae1f96e6
JulienDelplanque <julien.delplanque@live.be>
2021-06-25 22:15:48 +0200

    Make python interpreter easily configurable.

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+PYTHON="pypy3"
+
 SCRIPT_PATH=`realpath $0`
 SCRIPT_DIR=`dirname $SCRIPT_PATH`
 FORTHSRC_DIR="$SCRIPT_DIR/../forthsrc"
@@ -10,6 +12,6 @@ SNAPSHOT_SCRIPT="$SCRIPT_DIR/snapshot.f"
 BYE_SCRIPT="$SCRIPT_DIR/bye.f"
 GENERATED_IMAGE_NAME="memory.image"
 
-cat $@ "$SNAPSHOT_SCRIPT" "$BYE_SCRIPT" | pypy3 -m forthpie.eforth.eforth16bits bootstrap-run
+cat $@ "$SNAPSHOT_SCRIPT" "$BYE_SCRIPT" | $PYTHON -m forthpie.eforth.eforth16bits bootstrap-run
 
 mv "$GENERATED_IMAGE_NAME" "core.image"
